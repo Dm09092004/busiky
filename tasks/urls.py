4cbd5fe13eac53c3771aa3340f9e6497a15c9842
Dm09092004 <duc.momoducky@yandex.ru>
2026-06-02 02:04:41 +0300

    fix: Add basename parameter to router.register in tasks/urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet, TimeEntryViewSet, MyScheduleView
 
 router = DefaultRouter()
-router.register(r'tasks', TaskViewSet)
-router.register(r'time-entries', TimeEntryViewSet)
+router.register(r'tasks', TaskViewSet, basename='task')
+router.register(r'time-entries', TimeEntryViewSet, basename='timeentry')
 
 urlpatterns = [
     path('', include(router.urls)),
